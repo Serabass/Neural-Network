0ec5c345cd35bb0524ece8941a62566b3dcf8db6
Serabass <is_everything@mail.ru>
2018-07-01 16:45:50 +0600

    Init

---
+++ N.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace ANN
+{
+    public class N : Neuron<Bitmap>
+    {
+        public void LoadMemory()
+        {
+            memory = (Bitmap) Bitmap.FromFile(name);
+        }
+
+        public void DisposeMemory()
+        {
+            memory.Dispose();
+        }
+    }
+}
---
+++ Neuron.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace ANN
+{
+    public abstract class Neuron<T>
+    {
+        public string name;
+
+        public int output = 0;
+        public int weight = 0;
+        public T input;
+        public T memory;
+
+        public new string ToString()
+        {
+            return $"Name {name}; Weight: {weight}";
+        }
+    }
+}
